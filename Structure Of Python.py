5e98a1d42be855a5e96ea8a620bf38b34e5c9eb9
VaithiArul <107176027+VaithiArul@users.noreply.github.com>
2022-06-09 22:21:58 +0530

    Structures of Python

---
+++ Structure Of Python.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Integer:
+# 
+
+# In[11]:
+
+
+a = 5
+print("The value of a : ",a)
+print("The type of a : ",type(a))
+print("The Integer Value is :",isinstance(a,int))
+#Here if we change the "int" to other datatype, it checks weather it is that datatype or not
+
+
+# # Float:
+
+# In[2]:
+
+
+a = 10.5
+print("The value of a : ",a)
+print("The type of a : ",type(a))
+
+
+# # Complex:
+
+# In[19]:
+
+
+a = 4 + 10.5j
+b = 4 + 11.5j
+print("The value of a : ",a)
+print(a.real)
+print(a.imag)
+c = a + b
+print(c)
+print("The type of a : ",type(a))
+print("The Complex Value is :",isinstance(2 + 4j,complex))
+print("The Complex Value is :",isinstance(2 + 4,complex))
+
+
+# # List:
+
+# In[7]:
+
+
+list = [1,2,3,4,5]
+print(list)
+print("Type:",type(list))
+
+
+# # Tuple:
+
+# In[12]:
+
+
+a = (1,2,3)
+print(a)
+
+
+# # Boolean:
+
+# In[10]:
+
+
+a = True
+print(type(a))
+
+
+#  # Set:
+#  
+
+# In[11]:
+
+
+a = {1,2,3,4,5}
+print("The value of a : ",a)
+print("The type of a : ",type(a))
+
+
+# # Dictionary:
+
+# In[13]:
+
+
+a = {1:"Anime",2:"Naruto"}
+print("The value of a : ",a)
+print("The type of a : ",type(a))
+
+
+# In[ ]:
+
+
+
+
